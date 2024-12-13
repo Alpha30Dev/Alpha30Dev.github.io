9ae68349f0774632647b8d98ee687fbfea2424b8
Maxime <vandequin@gmail.com>
2024-12-13 12:48:34 +0100

    firstcommit

---
+++ snake.js
@@ -0,0 +1,110 @@
+const canvas = document.querySelector("canvas")
+const context = canvas.getContext('2d')
+
+let box = 20
+
+let snake = [];
+snake[0] = { x: 10*box, y: 10*box }
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+let score = 0
+let d
+let seconds = 59
+let gameOver = false
+
+document.addEventListener("keydown", direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if(key == 37 && d !="RIGHT"){
+        d = "LEFT";
+    } else if (key == 39 && d !="LEFT"){
+        d = "RIGHT";
+    } else if (key == 40 && d !="UP"){
+        d = "DOWN";
+    } else if (key == 38 && d !="DOWN"){
+        d = "UP";
+    }
+}
+
+function collision(head, array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+function draw(){
+    if (gameOver) {
+        clearInterval(game);
+        return;
+    }
+
+    context.clearRect(0, 0, 400, 400)
+    
+    for(let i = 0; i < snake.length; i++){
+        context.fillStyle = (i == 0) ? "green" : "lightgreen"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+        context.strokeStyle = "green"
+        context.strokeRect(snake[i].x, snake[i].y, box, box)
+    }
+    
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
+    
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+    
+    if(d == "LEFT") snakeX -= box
+    if(d == "UP") snakeY -= box
+    if(d == "RIGHT") snakeX += box
+    if(d == "DOWN") snakeY += box
+    
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        food = {
+            x: Math.floor(Math.random() * 15 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 1) * box
+        }
+    } else {
+        snake.pop()
+    }
+    
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    
+    if(snakeX < 0 || snakeX > 19*box || snakeY < 0 || snakeY > 19*box || collision(newHead, snake) || seconds === 0){
+        clearInterval(game);
+        gameOver = true;
+        return;
+    }
+    
+    snake.unshift(newHead)
+    
+    context.fillStyle = "red"
+    context.font = "30px Arial"
+    context.fillText(score, 2*box, 1.6*box)
+    
+    context.fillStyle = seconds < 11 ? "red" : "white"
+    context.font = "20px Arial"
+    context.fillText(`Temps: ${seconds > 9 ? "" : "0"}${seconds}s`, 14*box, 1.6*box)
+}
+
+function startTimer() {
+    setInterval(() => {
+        if (!gameOver && seconds > 0) {
+            seconds--;
+        }
+    }, 1000);
+}
+
+let game = setInterval(draw, 100);
+startTimer();
