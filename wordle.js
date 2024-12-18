d0a3dacd0deb64312894b8a8a2543a5271f91b35
Connor <connorsexton@hotmail.com>
2024-12-18 16:32:28 +0100

    The logic of the game is complete / functional (rules link, formatting, and intergration into wider game still need to be completed)

--- wordle.js
+++ wordle.js
@@ -1,16 +1,13 @@
-const currentPlayerDisplay = document.getElementById("current-player")
-const countDownDisplay = document.getElementById("count-down")
-const startBtn = document.getElementById("cs-start-button")
-const messages = document.getElementById("cs-messages")
-const clueP = document.getElementById("cs-clue-p")
+const gameContainer = document.getElementById("cs-game-container")
+const currentPlayerDisplay = document.getElementById("cs-current-player")
+const countDownDisplay = document.getElementById("cs-count-down")
+
 let inputForm = document.getElementById("cs-input-form")
 
-
-
+//DEVELOPMENT PLAYERS (TO BE DELETED)
 let players = [
     {
-        player: "Connor",
-        turn: "false",
+        name: "Connor",
         wordleScore: 0,
         penduScore: 0,
         snakeScore: 0,
@@ -18,8 +15,15 @@ let players = [
         twoThousandFortyEightScore: 0, 
     },
     {
-        player: "Soukaina",
-        turn: "false",
+        name: "Soukaina",
+        wordleScore: 0,
+        penduScore: 0,
+        snakeScore: 0,
+        demineurScore: 0,
+        twoThousandFortyEightScore: 0, 
+    },
+    {
+        name: "Thomas",
         wordleScore: 0,
         penduScore: 0,
         snakeScore: 0,
@@ -28,53 +32,61 @@ let players = [
     },
 ]
 
-localStorage.setItem("players", JSON.stringify(players))
-
-
+localStorage.setItem("t2p-players", JSON.stringify(players))
+//END OF DEVELOPMENT PLAYERS (TO BE DELETED)
 
 //GET PLAYER INFO FROM LOCAL
-const playersArrayJson = localStorage.getItem("players")
+const playersArrayJson = localStorage.getItem("t2p-players")
 const playersArray = JSON.parse(playersArrayJson)
-
-let playerCount = playersArray.length
-playerCount--
-
-//DISPLAY PLAYER 1
-currentPlayerDisplay.innerText = `Au tour de ${playersArray[0].player}`
 
 //GAME PLAY VARIABLES
 let word
 let clue
 let guessCount = 1
-let gameStarted = false
+let outOfGuesses = false
+let playerScore = 0
+let playerCount = 0
 
+//DISPLAY PLAYER 1
+currentPlayerDisplay.innerText = `Au tour de ${playersArray[playerCount].name}`
 
 //CLICK BUTTON CAlls the API
+const startBtnClick = async () => {
+    //GLOBAL VARIABLES AT THE START OF EACH GAME
+    // console.log("Player: ", playersArray[playerCount].name)
+    // console.log("playerCount: ", playerCount)
+    // console.log("playerScore ", playerScore)
+    // console.log("guessCount ", guessCount)
+    // console.log("outOfGuesses ", outOfGuesses)
 
-startBtn.addEventListener("click", async () => {
+    let clueP = document.getElementById("cs-clue-p")
+    let messages = document.getElementById("cs-messages")
     try {
         let data = await fetch(`https://trouve-mot.fr/api/size/5`)
         let wordObj = await data.json()
         word = wordObj[0].name 
         console.log(word)
         clue = wordObj[0].categorie
-        clueP.innerText = `Indice: ${clue} (ne deviner que des vrais mots de 5 lettres)`
-        gameStarted = true
+        clueP.innerText = `Indice: ${clue}`
+        outOfGuesses = false
         resetInputs()
-        //countDown()
+        countDown()
     } catch (error) {
         messages.innerText = `Il y avait une erreur: ${error.message}. Tenter de recharger la page.`
     }
-})
+    let startBtn = document.getElementById("cs-start-button")
+    startBtn.style.display = "none"
+}
 
-const formSubmit = async (event) => {
+const formSubmit = (event) => {
     event.preventDefault()
-   
+    
     let firstLetter = document.getElementById("cs-1-letter")
     let secondLetter = document.getElementById("cs-2-letter")
     let thirdLetter = document.getElementById("cs-3-letter")
     let fourthLetter = document.getElementById("cs-4-letter")
     let fifthLetter = document.getElementById("cs-5-letter")
+    let messages = document.getElementById("cs-messages")
 
     if (firstLetter.value.length !== 1 || secondLetter.value.length !== 1 || thirdLetter.value.length !== 1 || fourthLetter.value.length !== 1 || fifthLetter.value.length !== 1) {
         return
@@ -86,21 +98,10 @@ const formSubmit = async (event) => {
     
     //ENHANCEMENT FOR THE FUTURE - CHECK THE INPUT AGAINST A FRENCH LANGUAGE DICTIONARY TO BE SURE IT IS A REAL WORD (NO FRENCH LANGUAGE DICTIONARY APIs FOUND AT THE TIME OF DEVELOPEMENT) 
     //Emailed Collins to ask for free access to their api to check if the entry is a word - awaiting their response before incorperating this into the game
-    try { 
-        const response = await fetch(`https://fr.wiktionary.org/wiki/${guessWord}`);
-        console.log(response.status)
-	    const result = await response.json();
-	    console.log(result);
-    } catch (error) {
-        console.error(response.status, error);
-    } finally {
-        console.log(response.status)
-    }
 
-    //forEach letter in the guessWord array is it in the wordArray?
     let boxColors = []
     let wordScore = []
-    //COLOR CODE RESULT OF GUESS
+    
     for (let i = 0; i < guessWord.length; i++) {
         if (wordArray.includes(guessLetters[i])) {
             if (wordArray[i] === guessLetters[i]) {
@@ -112,7 +113,21 @@ const formSubmit = async (event) => {
             }  
         } else {
             boxColors.push("black")
+            wordScore.push(0)
         }
+    }
+
+    let playerScoreTemp = wordScore.reduce((a, b) => a + b)
+
+    if (playerScoreTemp === 1000) {
+        playerScore = playerScoreTemp
+        messages.innerText = `Vous avez trouvé le bon mot!` 
+        outOfGuesses = true
+        endGame()
+    } else if (playerScore > playerScoreTemp) {
+        playerScore = playerScore
+    } else {
+        playerScore = playerScoreTemp
     }
 
     let parentElementPs = document.getElementById(`cs-guess-${guessCount}`)
@@ -126,63 +141,138 @@ const formSubmit = async (event) => {
     `
     let parentElementInputsPosition = guessCount + 1
     let parentElementInputs = document.getElementById(`cs-guess-${parentElementInputsPosition}`)
-    console.log(parentElementInputsPosition)
-
-    parentElementInputs.innerHTML = `
-        <form id="cs-input-form" onsubmit="formSubmit(event)">
-            <label class="cs-labels" for="cs-1-letter">Premier lettre</label>
-            <input class="cs-display-box cs-inputs" id="cs-1-letter" type="text" maxlength="1" required>
-            <label class="cs-labels" for="cs-2-letter">Deuxieme lettre</label>
-            <input class="cs-display-box cs-inputs" id="cs-2-letter" type="text" maxlength="1" required>
-            <label class="cs-labels" for="cs-3-letter">Troisieme lettre</label>
-            <input class="cs-display-box cs-inputs" id="cs-3-letter" type="text" maxlength="1" required>
-            <label class="cs-labels" for="cs-4-letter">Quatrieme lettre</label>
-            <input class="cs-display-box cs-inputs" id="cs-4-letter" type="text" maxlength="1" required>
-            <label class="cs-labels" for="cs-5-letter">Cinqieme lettre</label>
-            <input class="cs-display-box cs-inputs" id="cs-5-letter" type="text" maxlength="1" required>
-            <input type="submit" value="Soumettre">
-        </form>
-    `
-
-    resetInputs()
     
-    console.log(boxColors)
-    guessCount++
-    if (guessCount === 6) {
-        //message "Out of guesses, the word was "${word}" "
-        console.log("the game is ended")
-        //endGame function
+    if (parentElementInputsPosition < 7 && playerScoreTemp !== 1000) {
+        parentElementInputs.innerHTML = `
+            <form id="cs-input-form" onsubmit="formSubmit(event)">
+                <label class="cs-labels" for="cs-1-letter">Premier lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-1-letter" type="text" maxlength="1" required>
+                <label class="cs-labels" for="cs-2-letter">Deuxieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-2-letter" type="text" maxlength="1" required>
+                <label class="cs-labels" for="cs-3-letter">Troisieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-3-letter" type="text" maxlength="1" required>
+                <label class="cs-labels" for="cs-4-letter">Quatrieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-4-letter" type="text" maxlength="1" required>
+                <label class="cs-labels" for="cs-5-letter">Cinqieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-5-letter" type="text" maxlength="1" required>
+                <input type="submit" value="Soumettre">
+            </form>
+        `   
+        resetInputs()
     }
 
+    guessCount++
+
+    if (guessCount === 7) {
+        messages.innerText = `Vous n'avez plus de tentatives!` 
+        outOfGuesses = true
+        endGame()
+    }
 }
 
+class TempPlayerScore {
+    constructor(name, score, time, timeRemaining) {
+        this.name = name,
+        this.score = score,
+        this.time = time,
+        this.timeRemaining = timeRemaining
+    }
+}
+
+let arrayOfPlayersScores = []
 
 const endGame = () => {
+    let messages = document.getElementById("cs-messages")
 
+    let tempPlayerScore = new TempPlayerScore(playersArray[playerCount].name, playerScore, (60 - seconds), seconds)
 
-    //
-    //"message - Your highest scoring guess was -> "
+    arrayOfPlayersScores.push(tempPlayerScore)
 
+    messages.innerText += `\nLe mot était "${word}". \nVotre score est ${playerScore} \n(Ce score sert uniquement à classer les joueurs une fois que tout le monde a joué.)`
+    
+    if (playerCount === playersArray.length - 1) {
+        //THIS BUTTON (MAYBE AN <a></a>) NEEDS TO BE THE LINK OUT OF WORDLE (EITHER TO FINAL SCORES OR DIRECT TO THE NEXT GAME)
+        gameContainer.innerHTML += `
+            <button onclick="tempFunction()")}" style="display: block">Passer au resultats finals de Wordle</button>
+        `
+        arrayOfPlayersScores.forEach((element) => {
+            gameContainer.innerHTML += `
+                \n<tr>
+                    <th style="font-weight: bold">Player: ${element.name},</th>
+                    <td>Score: ${element.score},</td>
+                    <td>Time: ${element.time}s</td>
+                </tr>
+            `
+        })
+        endWordle()
+    } else {
+        // create a button to "Load next player"
+        gameContainer.innerHTML += `
+            <button onclick="loadNewPlayer()" style="display: block">Passer au prochain jouer</button>
+        `
+        arrayOfPlayersScores.forEach((element) => {
+            gameContainer.innerHTML += `
+                \n<tr>
+                    <th style="font-weight: bold">Player: ${element.name},</th>
+                    <td>Score: ${element.score},</td>
+                    <td>Time: ${element.time}s</td>
+                </tr>
+            `
+        })  
+    }
+}
 
+//tempFunction for deleting
+const tempFunction = () => {
+    console.log("This is over baby!")
+}
 
-    // when guess count = 6 end game put in time display "Out of guesses"
-    // timer === 0 stop game regardless of guesses and // calculate score and stock in a class (display score)
-    // if playerCount = playersArray.length 
-    //calculate player rank
-    //update scores in localStorage and change turn for all players to false
-    //playersArray.forEach(element => element.turn = false)
-    //button becomes link to next game
-
-
-
+const endWordle = () => {
+    console.log("arrayOfPlayersScores", arrayOfPlayersScores)
+    
+    //SORT BY SCORE AND THEN TIME
+    arrayOfPlayersScores.sort((a, b) => {
+        if (a.score !== b.score) {
+            return a.score - b.score
+        } else {
+            return a.timeRemaining - b.timeRemaining
+        }
+    })
+    
+    //REVERSE ARRAY SO THAT HIGHEST PLAYER IS FIRST
+    
+    arrayOfPlayersScores.reverse()
+    
+    
+    let points = arrayOfPlayersScores.length
+    let samePoints = 1
+    
+    for (let i = 0; i < arrayOfPlayersScores.length; i++) {
+        if (i === 0) {
+            //ASSIGN THE FIRST PLAYER MAX POINTS
+            let index = playersArray.findIndex((element) => {return element.name === arrayOfPlayersScores[i].name})
+            playersArray[index].wordleScore = points
+        } else if (arrayOfPlayersScores[i].score === arrayOfPlayersScores[i - 1].score && arrayOfPlayersScores[i].timeRemaining === arrayOfPlayersScores[i - 1].timeRemaining) {
+            //IF THE CURRENT PLAYER HAS THE SAME SCORE AND TIME AS PREVIOUS PLAYER THEY GET THE SAME POINTS (KEEP TRACK OF HOW MANY TIMES THIS IS THE CASE WITH samePoints)
+            let index = playersArray.findIndex((element) => {return element.name === arrayOfPlayersScores[i].name})
+            playersArray[index].wordleScore = points
+            samePoints++
+        } else {
+            //CURRENT PLAYER HAS DIFFERENT SCORE OR TIME COMPARED WITH THE PREVIOUS
+            let index = playersArray.findIndex((element) => {return element.name === arrayOfPlayersScores[i].name})
+            playersArray[index].wordleScore = points - samePoints
+            //just add the score based on the array...
+            points = points - samePoints
+            samePoints = 1
+        }
+    }
+    
+    localStorage.clear()
+    localStorage.setItem("t2p-players", JSON.stringify(playersArray))
 }
 
 
-
     
-    
-
-
 
 //CODE FOR INPUT FOCUSING
 const resetInputs = () => {
@@ -205,26 +295,94 @@ const resetInputs = () => {
     })
 }
 
-
-
-
-
-
 let seconds
-const countDown = () => {    
+const countDown = () => {  
+    let messages = document.getElementById("cs-messages")  
     seconds = 59
     function tick () {
+        if (outOfGuesses) {
+            clearInterval(ticker) 
+            return
+        }
         seconds--
         countDownDisplay.innerText = `Time: ${seconds > 9 ? "" : "0"}${seconds}s` 
         seconds < 11 ? countDownDisplay.style.color = "red" : ""
         if (seconds === 0) {
             clearInterval(ticker)
-            //message "Out of time, the word was "${word}" "
-            //get inputs and make inputs.disabled = true
-            //endGame()
+            messages.innerText = `Vous n'avez plus de temps!` 
+            let inputs = document.querySelectorAll(".cs-inputs")
+            inputs.forEach(element => element.disabled = true)
+            endGame()
         }
     }
     let ticker = setInterval(tick, 1000)
 }
 
 
+const loadNewPlayer = () => {
+    guessCount = 1
+    playerCount++
+    playerScore = 0
+    outOfGuesses = false
+
+    let startBtn = document.getElementById("cs-start-button")
+    startBtn.style.display = "block"
+    console.log("Load New Player!")
+    currentPlayerDisplay.innerText = `Au tour de ${playersArray[playerCount].name}`
+    countDownDisplay.innerText = "Time: 1:00"
+    gameContainer.innerHTML = `
+        <p id="cs-clue-p"></p>
+        <p id="cs-messages"></p>
+        <div class="cs-word-container" id="cs-guess-1">
+            <form id="cs-input-form" onsubmit="formSubmit(event)">
+                <label class="cs-labels" for="cs-1-letter">Premier lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-1-letter" type="text" maxlength="1" required disabled="true">
+                <label class="cs-labels" for="cs-2-letter">Deuxieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-2-letter" type="text" maxlength="1" required disabled="true">
+                <label class="cs-labels" for="cs-3-letter">Troisieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-3-letter" type="text" maxlength="1" required disabled="true">
+                <label class="cs-labels" for="cs-4-letter">Quatrieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-4-letter" type="text" maxlength="1" required disabled="true">
+                <label class="cs-labels" for="cs-5-letter">Cinqieme lettre</label>
+                <input class="cs-display-box cs-inputs" id="cs-5-letter" type="text" maxlength="1" required disabled="true">
+                <input type="submit" value="Soumettre">
+            </form>
+        </div>
+        <div class="cs-word-container" id="cs-guess-2">
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+        </div>
+        <div class="cs-word-container" id="cs-guess-3">
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+        </div>
+        <div class="cs-word-container" id="cs-guess-4">
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+        </div>
+        <div class="cs-word-container" id="cs-guess-5">
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+        </div>
+        <div class="cs-word-container" id="cs-guess-6">
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+            <p class="cs-display-box-p"></p>
+        </div>
+        <button id="cs-start-button" onclick="startBtnClick()">Cliquer pour commencer le jeu</button>
+    `
+}
